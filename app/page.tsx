df734572a4b798876abc1fcb2f95369a6f6feb73
Khushbutara <khushbutarashekh3@gmail.com>
2025-12-17 16:56:57 +0530

    each button redirect to specific quiz_id

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,37 @@
-import { getLandingPage } from '../lib/api';
+import { getLandingPage, getQuizList } from '../lib/api';
 import Hero from '../components/landing/Hero';
-import Benefits from '../components/landing/Benefits';
 import Goals from '../components/landing/Goal';
 import CTA from '../components/landing/CTA';
 import SiteHeader from '@/components/layouts/SiteHeader';
 
 export default async function HomePage() {
   const data = await getLandingPage();
+  const quizListRes = await getQuizList();
+
+  const quizMap: Record<string, number> = {};
+  quizListRes.quizzes.forEach((quiz: any) => {
+    quizMap[
+      quiz.title.toLowerCase().replace(/\s+/g, '_')
+    ] = quiz.id;
+  });
 
   return (
     <main className="bg-[#E5391C] text-white relative overflow-hidden">
-  <section className="relative">
-    <SiteHeader />
-  
-    {/* <Hero
-      heroTitle={data.hero_title}
-      heroSubtitle={data.hero_subtitle}
-      heroDescription={data.hero_description}
-    />
-    <Benefits benefits={data.benefits} /> */}
-    <Hero
-  heroTitle={data.hero_title}
-  heroSubtitle={data.hero_subtitle}
-  heroDescription={data.hero_description}
-  benefits={data.benefits}
-/>
+      <SiteHeader />
 
-  </section>
+      <Hero
+        heroTitle={data.hero_title}
+        heroSubtitle={data.hero_subtitle}
+        heroDescription={data.hero_description}
+        benefits={data.benefits}
+      />
 
-  <CTA text={data.cta_button_text} />
-  <Goals goals={data.goal_options} />
-</main>
+      <CTA text={data.cta_button_text} />
+
+      <Goals
+        goals={data.goal_options}
+        quizMap={quizMap}   
+      />
+    </main>
   );
 }
