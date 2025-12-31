cc0c04d829c84a45e2ddd67a98e913b1a1a98db3
Dynamic <support@dynamicdreamz.com>
2025-12-31 12:39:01 +0530

    Testimonial section design changes

--- components/book/TestimonialSection.tsx
+++ components/book/TestimonialSection.tsx
@@ -183,15 +183,15 @@ export default function TestimonialSection({ data }: Props) {
       >
         <Slider ref={sliderRef} {...settings} className="testimonial-slider">
           {data.testimonials.map((t, i) => (
-            <div key={i} className="px-4 h-full">
-              <div className="bg-[#4a4a4a] rounded-[5px] p-6 border-r-4 border-b-4 border-white/20 flex flex-col justify-between hover:bg-[#555555] transition h-full">
+            <div key={i} className="px-4 h-full md:min-h-[354px]">
+              <div className="bg-[#4a4a4a] rounded-[5px] sm:p-8.5 p-6 border-r-4 border-b-4 border-white/20 flex flex-col justify-between hover:bg-[#555555] transition h-full">
                 <div className="flex gap-1 text-[#FEE106] mb-2 text-2xl">
                   {Array.from({ length: Number(t.rating) }).map((_, i) => (
                     <span key={i}>★</span>
                   ))}
                 </div>
 
-                <h3 className="font-semibold mb-2.5 text-lg">{t.title}</h3>
+                <h3 className="font-semibold mb-2.5 sm:text-lg text-md">{t.title}</h3>
 
                 <div
                   className="text-sm xl:text-lg sm:text-base text-[#FFFFFFCC] leading-relaxed mb-3"
@@ -199,7 +199,7 @@ export default function TestimonialSection({ data }: Props) {
                 />
 
                 <div>
-                  <p className="font-semibold">{t.author}</p>
+                  <p className="font-semibold sm:text-lg text-md">{t.author}</p>
                   <p className="text-sm text-gray-300">{t.author_city}</p>
                 </div>
               </div>
