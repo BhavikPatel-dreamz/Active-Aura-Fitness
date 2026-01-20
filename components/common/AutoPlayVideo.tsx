efd273f9240caf7cbb28955c5dcaf2bb46b4485b
Khushbutara <khushbutarashekh3@gmail.com>
2026-01-20 11:16:54 +0530

    autoplay added

--- components/common/AutoPlayVideo.tsx
+++ components/common/AutoPlayVideo.tsx
@@ -10,39 +10,64 @@ export default function AutoplayVideo({
   className?: string;
 }) {
 
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+  // const videoRef = useRef<HTMLVideoElement | null>(null);
 
-  useEffect(() => {
-    const video = videoRef.current;
-    if (!video) return;
+  // useEffect(() => {
+  //   const video = videoRef.current;
+  //   if (!video) return;
 
-    // Try autoplay muted (required by browsers)
-    video.muted = true;
-    video.play().catch(() => {});
+  //   // Try autoplay muted (required by browsers)
+  //   video.muted = true;
+  //   video.play().catch(() => {});
 
-    // After user interaction → unmute + keep playing
-    const enableSound = () => {
-      video.muted = false;
-      video.play().catch(() => {});
-      window.removeEventListener("click", enableSound);
-      window.removeEventListener("touchstart", enableSound);
-      window.removeEventListener("scroll", enableSound);
-    };
+  //   // After user interaction → unmute + keep playing
+  //   const enableSound = () => {
+  //     video.muted = false;
+  //     video.play().catch(() => {});
+  //     window.removeEventListener("click", enableSound);
+  //     window.removeEventListener("touchstart", enableSound);
+  //     window.removeEventListener("scroll", enableSound);
+  //   };
 
-    window.addEventListener("click", enableSound);
-    window.addEventListener("touchstart", enableSound);
-    window.addEventListener("scroll", enableSound);
-  }, []);
+  //   window.addEventListener("click", enableSound);
+  //   window.addEventListener("touchstart", enableSound);
+  //   window.addEventListener("scroll", enableSound);
+  // }, []);
 
   return (
     <video
-      ref={videoRef}
+      // ref={videoRef}
       src={src}
       loop
       playsInline
       controls
+      autoPlay
+      muted
       preload="auto"
       className={className}
     />
   );
 }
+
+// "use client";
+
+// export default function AutoplayVideo({
+//   src,
+//   className = "",
+// }: {
+//   src: string;
+//   className?: string;
+// }) {
+//   return (
+//     <video
+//       src={src}
+//       autoPlay
+//       muted
+//       loop
+//       playsInline
+//       preload="auto"
+//       className={className}
+//     />
+//   );
+// }
+
