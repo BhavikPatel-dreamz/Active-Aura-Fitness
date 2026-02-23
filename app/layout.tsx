0c8ae5d0794837630084b60b57b1193fd923a09a
Khushbutara <khushbutarashekh3@gmail.com>
2026-02-23 15:08:06 +0530

    pixel script is added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Bebas_Neue, Poppins } from 'next/font/google';
 import { getLogos, getFavicon} from '@/lib/api';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -70,8 +71,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+   const PIXEL_ID = "517991158551582";
   return (
     <html lang="en">
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+            fbq('track', 'PageView');
+            
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${bebasNeue.variable} ${poppins.variable} antialiased`}
       >
